5e34f66965b082152f4ee6bf665e2d6c8a929458
unknown <tiokazami@gmail.com>
2023-05-13 01:55:51 +0700

    add file connect.js

---
+++ connect.js
@@ -0,0 +1,15 @@
+let mysql = require('mysql');
+
+// create connected database
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dbrestapi"
+});
+
+conn.connect((err) => {
+    if (err) throw err;
+    console.log("Mysql Connected")
+})
---
+++ server.js
@@ -0,0 +1,11 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+// parse aplication/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+});
